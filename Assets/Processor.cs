04cd199fa5ebf4f2d00eb0725e2e5848a0ae71c9
Saphirah <nomercyblackwood@gmail.com>
2023-08-21 17:29:57 +0200

    Cleanup Files

--- Assets/Processor.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-public abstract class Processor
-{
-    protected readonly UnityTogetherClient communication;
-    protected Processor(UnityTogetherClient com) => communication = com;
-}
